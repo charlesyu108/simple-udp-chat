4557fd78e9760ddcc27754672b41183fcde2fe5a
Charles Yu <charles.yu108@gmail.com>
2021-01-16 00:07:52 -0800

    Rename some things

--- udp-chat.py
+++ udp-chat.py
@@ -3,21 +3,20 @@ import threading
 import time
 from dataclasses import dataclass
 
-
 @dataclass
 class Message:
     user: str
     text: str
 
-    def ser(self):
+    def serialize(self) -> bytes:
         return f"{self.user}#{self.text}".encode('utf-8')
-    
+
     @staticmethod
-    def deser(data):
+    def deserialize(data: bytes) -> "Message":
         user, text = data.decode('utf-8').split("#")
         return Message(user, text)
 
-    def fmtstring(self):
+    def fmt_string(self) -> str:
         return f"[{self.user}] {self.text}"
 
 
@@ -27,27 +26,27 @@ class ChatSession:
     port: int
 
     def prompt_and_send_message(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as send_sock:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as send_sock:
             send_sock.settimeout(0.2)
             send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             while True:
                 msg_text = input("> ")
                 msg = Message(self.user, msg_text)
-                send_sock.sendto(msg.ser(), ("<broadcast>", self.port))
-                time.sleep(1.0)
+                send_sock.sendto(msg.serialize(), ("<broadcast>", self.port))
+                time.sleep(0.2)
 
     def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as recv_sock:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as recv_sock:
             recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             recv_sock.bind(("", self.port))
             while True:
                 data, addr = recv_sock.recvfrom(1024)
                 if data:
-                    msg = Message.deser(data)
-                    print(msg.fmtstring())
-                time.sleep(1.0)
+                    msg = Message.deserialize(data)
+                    print(msg.fmt_string())
+                time.sleep(0.2)
 
     def start(self):
         listening_thread = threading.Thread(target=self.listen)
@@ -59,6 +58,6 @@ class ChatSession:
 
 if __name__ == "__main__":
     user_name = input("Please input a user name: ")
-    port = int(input("Where would you like to connect?: "))
-    session = ChatSession(user_name, port=port)
+    port = int(input("What port would you like to connect to?: "))
+    session = ChatSession(user_name, port)
     session.start()
